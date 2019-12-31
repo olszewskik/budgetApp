77a8879352bf104053cc4ee5b333e37464cb8dcb
Kamil Olszewski <kamil.web@gmail.com>
2019-12-31 17:33:56 +0100

    budgetApp

--- src/views/Root.js
+++ src/views/Root.js
@@ -2,6 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from '../components/Header';
 import AppRouter from '../router/AppRouter';
+import configureStore from '../store/configureStore';
+import { addExpense } from '../actions/expenses';
+import { setTextFilter } from '../actions/filters';
+import getVisibleExpenses from '../selectors/expenses';
+
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'water bill' }));
+store.dispatch(addExpense({ description: 'gas bill' }));
+store.dispatch(setTextFilter('water'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+
+console.log(visibleExpenses);
 
 function Root() {
   return (
