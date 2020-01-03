48f3e76ad072e723b9f655978a943825e721fdd2
Kamil Olszewski <kamil.web@gmail.com>
2020-01-03 21:01:50 +0100

    budgetApp

---
+++ src/firebase/index.sample.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+
+export { firebase, database as default };
